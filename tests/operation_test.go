e8dfc5a86ad432ce4cd932665fe427246fe8b070
MonkieeBoi <danny.pham26425@gmail.com>
2025-05-25 13:31:51 +1000

    test: operation

--- tests/operation_test.go
+++ tests/operation_test.go
@@ -52,3 +52,33 @@ func TestOperationShape5(t *testing.T) {
 	op := NewOperation(M_Z, R_LEFT, 2, 1)
 	printShapeErr(t, want, op.Shape())
 }
+
+func TestOperationShape6(t *testing.T) {
+	want := [][2]int{{4,10},{4,9},{4,11},{3,10}}
+	op := NewOperation(M_T, R_LEFT, 4, 10)
+	printShapeErr(t, want, op.Shape())
+}
+
+func TestOperationShape7(t *testing.T) {
+	want := [][2]int{{4,7},{4,8},{3,7},{3,8}}
+	op := NewOperation(M_O, R_LEFT, 4, 7)
+	printShapeErr(t, want, op.Shape())
+}
+
+func TestOperationShape8(t *testing.T) {
+	want := [][2]int{{8,16},{8,15},{8,17},{7,15}}
+	op := NewOperation(M_J, R_LEFT, 8, 16)
+	printShapeErr(t, want, op.Shape())
+}
+
+func TestOperationShape9(t *testing.T) {
+	want := [][2]int{{6,9},{6,8},{6,10},{5,10}}
+	op := NewOperation(M_L, R_LEFT, 6, 9)
+	printShapeErr(t, want, op.Shape())
+}
+
+func TestOperationShape10(t *testing.T) {
+	want := [][2]int{{4,2},{4,1},{4,3},{3,2}}
+	op := NewOperation(M_T, R_LEFT, 4, 2)
+	printShapeErr(t, want, op.Shape())
+}
